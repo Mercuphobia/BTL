8ef87e3ab895c403e4f0233b79d11dee5848ae1c
Mercuphobia <trungbia2002@gmail.com>
2024-08-21 00:34:27 -0700

    update21/08_3

--- include/parsers_option.h
+++ include/parsers_option.h
@@ -2,7 +2,7 @@
 #define PARSER_OPTION_H
 void parsers_option(int argc, char *argv[]);
 
-extern int stop_execution; // khai bao bien toan cuc 
+extern int stop_execution; // khai bao bien toan cuc de dung o cac file khac
 
 #endif // PARSER_OPTION_H
 
